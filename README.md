b7cd00010572d1a5e8e29d0d18c3ad76e03c4229
Manuel M <manu4216@gmail.com>
2017-03-26 22:30:54 +0000

    Finish first working version of the exercise

---
+++ README.md
@@ -0,0 +1,14 @@
+## Timestamp Microservice
+
+Objective: Build a full stack JavaScript app that is functionally similar to this: <https://timestamp-ms.herokuapp.com/> and deploy it to Heroku.
+
+
+**User Stories:**
+
+ 1. I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date *(example: January 1, 2016).*
+
+ 2. If it does, it returns both the Unix timestamp and the natural language form of that date.
+
+ 3. If it does not contain a date or Unix timestamp, it returns null for those properties.
+ 
+ *[Herman Fassett](https://freecodecamp.com/hermanfassett)*
--- server.js
+++ server.js
@@ -1,8 +1,70 @@
 var express = require('express')
+var path = require('path')
+var dateFormat = require('dateformat')
 var app = express()
 
+/** 
+ * INPUT: root path
+ * OUTPUT: static file: 'index.html'
+ */
 app.get('/', function (req, res) {
-  res.send('Hello Bebelush and poodle puppy!')
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+/** 
+ * INPUT: any path matching the given pattern
+ * OUTPUT: static file: 'index.html'
+ */
+app.get('/*/*', function (req, res) {
+    res.send('You can only use URLs with the following format: url/date, eg url/1234567890')
+})
+
+
+/**
+ * Gets the date in both 'unix timestamp' and 'natural' format
+ */
+function getDateObject (rawDate) {
+    var nativeJSDate, unixDate, naturalDate
+    
+    rawDate = unescape(rawDate)
+    
+    // Use isNaN to check if number: isNaN(123) and isNaN('123') are 'false'
+    if (isNaN(rawDate)) {
+        nativeJSDate = new Date(rawDate)
+    } else {
+        nativeJSDate = new Date(parseInt(rawDate))
+    }
+    
+    if (!nativeJSDate || nativeJSDate == 'Invalid Date') {
+        return {
+            unix: null,
+            natural: null
+        }
+    }
+    
+    naturalDate = dateFormat(nativeJSDate, "mmmm dd, yyyy") || null
+    unixDate = nativeJSDate.getTime() || null
+    
+    return {
+        unix: unixDate,
+        natural: naturalDate
+    }
+}
+
+
+/** 
+ * INPUT: date 
+ *    - valid date formats: a) unix (eg: 12345678)
+ *                          b) natural language (eg: "December 15, 2015") 
+ * OUTPUT: object (eg: { "unix": 1450137600, "natural": "December 15, 2015" })
+ *    - if date is valid: { "unix": 1450137600, "natural": "December 15, 2015" })
+ *    - if date is invalid: { "unix": null, "natural": null })
+ */
+app.get('/:date', function (req, res) {
+    var date = req.params.date,
+        answer = getDateObject(date)
+    
+    res.send(answer)
 })
 
 app.listen(8080, function () {
