0e02831410419089434e009eaac81dcf7d065cd3
Manuel M <manu4216@gmail.com>
2017-03-27 22:14:46 +0000

    Render the port dynamic

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var path = require('path')
 var dateFormat = require('dateformat')
 var app = express()
 
+app.set('port', process.env.PORT || 5000)
+
 app.use(express.static(__dirname + '/public'))
 
 /** 
@@ -69,6 +71,6 @@ app.get('/:date', function (req, res) {
     res.send(answer)
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port + ' + app.get('port'))
 })
